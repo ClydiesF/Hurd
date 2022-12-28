8b018df274f874fb617f03ca933dea2799222407
Clydies Freeman III <c.edward.freeman@gmail.com>
2022-12-27 22:54:15 -0600

    Initial Commit

---
+++ Hurd/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Hurd
+//
+//  Created by clydies freeman on 12/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hurd/HurdApp.swift
@@ -0,0 +1,17 @@
+//
+//  HurdApp.swift
+//  Hurd
+//
+//  Created by clydies freeman on 12/27/22.
+//
+
+import SwiftUI
+
+@main
+struct HurdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
