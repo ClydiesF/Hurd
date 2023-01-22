507776c7d3177cb18b664713210ca4537259e51c
Clydies Freeman III <c.edward.freeman@gmail.com>
2023-01-21 22:44:41 -0600

    Add Function to edit Profile Values

---
+++ Hurd/View/EditProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  EditProfileView.swift
+//  Hurd
+//
+//  Created by clydies freeman on 1/21/23.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditProfileView()
+    }
+}
