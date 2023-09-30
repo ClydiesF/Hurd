b598790243847044f794c4f27c18f24d74c649bc
Clydies Freeman III <c.edward.freeman@gmail.com>
2023-09-29 21:49:23 -0500

    commited latest changes

--- Hurd/View/Onboarding/SigninOptionsView.swift
+++ Hurd/View/Onboarding/SigninOptionsView.swift
@@ -13,106 +13,114 @@ struct SigninOptionsView: View {
     @Environment(\.authorizationController) private var authorizationController
     
     var body: some View {
-        ScrollView {
-            VStack(spacing: Spacing.sixteen) {
-                
-                Image("hurdLogo")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 40)
-                
-                
-                Text("Welcome to hurd 👋")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .foregroundStyle(
-                        LinearGradient(
+        NavigationStack {
+            ScrollView {
+                VStack(spacing: Spacing.sixteen) {
+                    
+                    Image("hurdLogo")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 40)
+                    
+                    
+                    Text("Welcome to hurd 👋")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundStyle(
+                            LinearGradient(
+                                colors: [Color(hex: "099773"), Color(hex: "43b692")],
+                                startPoint: .leading,
+                                endPoint: .trailing
+                            )
+                        )
+                    
+                    Text(" Where the social Trip collaboration empowers all")
+                        .multilineTextAlignment(.center)
+                        .font(.subheadline)
+                        .padding(.bottom, Spacing.sixteen)
+                    
+                    Button(action: { AuthenticationMethods().handleGoogleSignIn() }, label: {
+                        SignInOptionView(image: "googleLogo", text: "Sign up with Google")
+                    })
+                    
+                    SignInWithAppleButtonView(vm: vm, signinType: .signup)
+                    
+    //                Button(action: {
+    //                    Task {
+    //                        do {
+    //                            let appleIDProvider = ASAuthorization
+    //                            let request = appleIDProvider.createRequest()
+    //                            request.requestedScopes = [.fullName, .email]
+    //
+    //                            let result = try await authorizationController
+    //                                .performRequests(request)
+    //
+    //                            switch result {
+    //                                // code to handle the authorization result
+    //                            }
+    //                        } catch {
+    //                            // code to handle the authorization error
+    //                        }
+    //                    }
+    //                }, label: {
+    //                    SignInOptionView(image: "appleLogo", text: "Sign up with Apple")
+    //                })
+                    
+                    NavigationLink {
+                        SignUpView()
+                    } label: {
+                        SignInOptionView(image: "envelope.fill", text: "Sign up with Email", isSystemImage: true)
+                    }
+
+    //                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+    //                    SignInOptionView(image: "envelope.fill", text: "Sign up with Email", isSystemImage: true)
+    //                })
+                    
+                    
+                    //                SignInOptionView(image: "person.fill.questionmark", text: "Continue as Anonymous", isSystemImage: true)
+                    HStack(spacing: Spacing.eight) {
+                        VStack {
+                            Divider()
+                        }
+                        
+                        Text("OR")
+                            .foregroundStyle(.gray.opacity(0.5))
+                        VStack {
+                            Divider()
+                        }
+                    }
+                    
+                    Text("Log into my account")
+                        .frame(height: 40)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(RoundedRectangle(cornerRadius: 30).fill(  LinearGradient(
                             colors: [Color(hex: "099773"), Color(hex: "43b692")],
                             startPoint: .leading,
                             endPoint: .trailing
-                        )
-                    )
-                
-                Text(" Where the social Trip collaboration empowers all")
-                    .multilineTextAlignment(.center)
-                    .font(.subheadline)
-                    .padding(.bottom, Spacing.sixteen)
-                
-                Button(action: { AuthenticationMethods().handleGoogleSignIn() }, label: {
-                    SignInOptionView(image: "googleLogo", text: "Sign up with Google")
-                })
-                
-                SignInWithAppleButtonView(vm: vm, signinType: .signup)
-                
-//                Button(action: {
-//                    Task {
-//                        do {
-//                            let appleIDProvider = ASAuthorization
-//                            let request = appleIDProvider.createRequest()
-//                            request.requestedScopes = [.fullName, .email]
-//                            
-//                            let result = try await authorizationController
-//                                .performRequests(request)
-//                            
-//                            switch result {
-//                                // code to handle the authorization result
-//                            }
-//                        } catch {
-//                            // code to handle the authorization error
-//                        }
-//                    }
-//                }, label: {
-//                    SignInOptionView(image: "appleLogo", text: "Sign up with Apple")
-//                })
-                
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                    SignInOptionView(image: "envelope.fill", text: "Sign up with Email", isSystemImage: true)
-                })
-                
-                
-                //                SignInOptionView(image: "person.fill.questionmark", text: "Continue as Anonymous", isSystemImage: true)
-                HStack(spacing: Spacing.eight) {
-                    VStack {
-                        Divider()
-                    }
+                        )))
                     
-                    Text("OR")
-                        .foregroundStyle(.gray.opacity(0.5))
-                    VStack {
-                        Divider()
+                    Spacer()
+                    
+                    
+                    Group {
+                        Text("By continuing you agree to Hurd's ") +
+                        Text("Privacy Policy ")
+                            .fontWeight(.semibold)
+                            .font(.system(size: 14))
+                            .foregroundColor(.bottleGreen) +
+                        Text("and ") +
+                        Text("terms of Service")
+                            .fontWeight(.semibold)
+                            .font(.system(size: 14))
+                            .foregroundColor(.bottleGreen)
                     }
+                    .font(.system(size: 13))
+                    
                 }
-                
-                Text("Log into my account")
-                    .frame(height: 40)
-                    .foregroundColor(.white)
-                    .frame(maxWidth: .infinity)
-                    .padding()
-                    .background(RoundedRectangle(cornerRadius: 30).fill(  LinearGradient(
-                        colors: [Color(hex: "099773"), Color(hex: "43b692")],
-                        startPoint: .leading,
-                        endPoint: .trailing
-                    )))
-                
-                Spacer()
-                
-                
-                Group {
-                    Text("By continuing you agree to Hurd's ") +
-                    Text("Privacy Policy ")
-                        .fontWeight(.semibold)
-                        .font(.system(size: 14))
-                        .foregroundColor(.bottleGreen) +
-                    Text("and ") +
-                    Text("terms of Service")
-                        .fontWeight(.semibold)
-                        .font(.system(size: 14))
-                        .foregroundColor(.bottleGreen)
-                }
-                .font(.system(size: 13))
-                
+                .padding()
             }
-            .padding()
         }
     }
 }
